ade3ef55e59ace1fd1e18e7b74219c6c54797892
Maryjane Watanabe <maryjanekw@gmail.com>
2025-09-23 15:34:01 -0700

    My nice folder stuctrure

---
+++ workbook1d/helloworld/src/main/java/com/pluralsight/helloworld/Main.java
@@ -0,0 +1,9 @@
+package com.pluralsight.helloworld;
+
+public class Main {
+        public static void main(String[] args) {
+            System.out.println("Hello, Bootcamp!");
+        }
+    }
+
+
