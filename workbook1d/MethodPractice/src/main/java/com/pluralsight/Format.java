0e2d5dd1d0663778749bb232365902d09c15aeae
Maryjane Watanabe <maryjanekw@gmail.com>
2025-09-26 13:32:37 -0700

    class exercise

---
+++ workbook1d/MethodPractice/src/main/java/com/pluralsight/Format.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class Format {
+    public static void main (String[] args) {
+        String first = "Maryjane";
+        String last = "Watanabe";
+        formatName();
+
+    }
+     public static void formatName(){
+
+
+    }
+}
