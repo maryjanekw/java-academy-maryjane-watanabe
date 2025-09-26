41405e1ddfd27fe5a78ca4e6fd1f7427c235b739
Maryjane Watanabe <maryjanekw@gmail.com>
2025-09-26 11:37:36 -0700

    menu exercise complete

---
+++ workbook1d/MethodPractice/src/main/java/com/pluralsight/Menu.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class Menu {
+    public static void main (String[] args) {
+        displayMenu();
+    }
+
+     public static void displayMenu(){
+         System.out.println("===MENU===");
+         System.out.println("1. Coffee - $3.99");
+         System.out.println("2. Tea - $2.99");
+         System.out.println("3. Cookie - $1.99");
+    }
+}
