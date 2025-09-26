4e400a90ceb6275856df92d1abc68fc74cd384b1
Maryjane Watanabe <maryjanekw@gmail.com>
2025-09-26 11:27:04 -0700

    ex. 1 complete

---
+++ workbook1d/MethodPractice/src/main/java/com/pluralsight/Hello.java
@@ -0,0 +1,23 @@
+package com.pluralsight;
+
+public class Hello {
+    public static void main(String[] args) {
+        hello();
+        goodbye();
+        goodMorning();
+    }
+
+    public static void hello() {
+        System.out.println("Hello, World!");
+    }
+
+    public static void goodbye() {
+        System.out.println("Goodbye!");
+    }
+
+    public static void goodMorning(){
+        System.out.println("Good morning!");
+    }
+}
+
+
