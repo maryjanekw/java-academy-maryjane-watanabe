3132c54b46ce71bfdbc6a421bf6d8f6ca79d167a
Maryjane Watanabe <maryjanekw@gmail.com>
2025-10-28 12:00:04 -0700

    exercise 1 Vehicles

---
+++ workbook-5/Vehicles/src/main/java/com/pluralsight/Vehicle.java
@@ -0,0 +1,41 @@
+package com.pluralsight;
+
+public class Vehicle {
+
+    public String color;
+    public int numberOfPassengers;
+    public int cargoCapacity;
+    public int fuelCapacity;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getNumberOfPassengers() {
+        return numberOfPassengers;
+    }
+
+    public void setNumberOfPassengers(int numberOfPassengers) {
+        this.numberOfPassengers = numberOfPassengers;
+    }
+
+    public int getFuelCapacity() {
+        return fuelCapacity;
+    }
+
+    public void setFuelCapacity(int fuelCapacity) {
+        this.fuelCapacity = fuelCapacity;
+    }
+
+    public int getCargoCapacity() {
+        return cargoCapacity;
+    }
+
+    public void setCargoCapacity(int cargoCapacity) {
+        this.cargoCapacity = cargoCapacity;
+    }
+}
